c6f2b795184215dc889ab016a0058c40c2b88fb7
hyl <411136528@qq.com>
2016-11-02 09:36:25 +0800

    圆形比例图

--- app/src/main/java/com/hyl/mis/hylviewdemo/MainActivity.java
+++ app/src/main/java/com/hyl/mis/hylviewdemo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.hyl.mis.hylviewdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class MainActivity extends Activity {
@@ -13,5 +15,11 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    public void jumpActivity2(View v){
+        Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(intent);
     }
 }
