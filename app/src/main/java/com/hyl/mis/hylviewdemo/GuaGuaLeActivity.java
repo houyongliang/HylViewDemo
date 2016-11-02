29c620df1fd3cef6b0c544e76acce098c9216ce9
hyl <411136528@qq.com>
2016-11-02 09:32:42 +0800

    1234

---
+++ app/src/main/java/com/hyl/mis/hylviewdemo/GuaGuaLeActivity.java
@@ -0,0 +1,13 @@
+package com.hyl.mis.hylviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuaGuaLeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gua_gua_le);
+    }
+}
